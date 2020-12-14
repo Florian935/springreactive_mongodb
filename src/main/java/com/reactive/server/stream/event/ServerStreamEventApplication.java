f79999ad5efd9a1a23bfa659f1b090adad257aae
Florian <florian.martin63000@gmail.com>
2020-12-14 23:00:55 +0100

    Initial commit

---
+++ src/main/java/com/reactive/server/stream/event/ServerStreamEventApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.server.stream.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerStreamEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerStreamEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactive/server/stream/event/ServerStreamEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.server.stream.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerStreamEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
